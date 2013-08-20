27740fa8a1ebffe5426a3b5ee7babe418cf48299
Valentin Lab <valentin.lab@kalysto.org>
2013-08-20 11:44:12 +0200

    fix: pkg: avoid looping whenever ``autogen.sh`` fails to modify ``setup.py``.

--- setup.py
+++ setup.py
@@ -12,13 +12,23 @@ if "%%short-version%%".startswith("%%"):
             "Please ensure ``./autogen.sh`` exists and that you are running "
             "``setup.py`` from the project root directory.\n")
         sys.exit(1)
-    os.system('./autogen.sh')
+    if os.path.exists('.autogen.sh.output'):
+        sys.stderr.write(
+            "It seems that ``./autogen.sh`` couldn't do its job as expected.\n"
+            "Please try to launch ``./autogen.sh`` manualy, and send the results to "
+            "the\nmaintainer of this package.\n"
+            "Package will not be installed !\n")
+        sys.exit(1)
+    sys.stderr.write("Missing version information: running './autogen.sh'...\n")
+    os.system('./autogen.sh > .autogen.sh.output')
     cmdline = sys.argv[:]
     if cmdline[1] == "install":
         ## XXXvlab: for some reason, this is needed when launched from pip
         if cmdline[0] == "-c":
             cmdline[0] = "setup.py"
-        sys.exit(subprocess.call(["python", ] + cmdline))
+        errlvl = subprocess.call(["python", ] + cmdline)
+        os.unlink(".autogen.sh.output")
+        sys.exit(errlvl)
 
 description_files = [
     'README.rst',
